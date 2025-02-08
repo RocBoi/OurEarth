b6f27f7bc3d5bf545f994b01a8a5ff78cf61d311
RocBoi <rocboiquezworld2@gmail.com>
2025-02-08 17:21:18 -0500

    index.js
    application routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const climateController = require('../controllers/climateController');
 
+// Define a route to get climate data
+router.get('/climate', climateController.getClimateData);
+
+module.exports = router;
+
+ 
